5a39d3b9586cb1b6aea41120bfa77f1d72ee183b
lucasblazzi <lucasblazzi@hotmail.com>
2021-12-14 01:26:46 -0300

    Set https + cors angular

--- wineClub/server.js
+++ wineClub/server.js
@@ -1,9 +1,11 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors())
 app.use(express.static('./dist/wineClub'));
 
 app.get('/*', (req, res) =>
